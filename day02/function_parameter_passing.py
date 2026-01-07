ecd5f9bf06aab1173f012a4eab0b504fb0cbd0f1
apple <1483258276@qq.com>
2026-01-07 20:23:20 +0800

    python函数：参数绑定详解：位置参数，默认参数，可变长参数，可变长关键字参数

--- day02/function_parameter_passing.py
+++ day02/function_parameter_passing.py
@@ -9,4 +9,134 @@ python的参数绑定有四种方式：
 4.可变关键字参数（**kwargs）收集剩余关键字实参。 (可变关键字参数)
 """
 
+# 1.位置参数的使用
+"""
+(1) 位置参数（Positional Parameters）
+最简单：必须按顺序提供实参。
+绑定方式：按位置从左到右匹配。
+"""
 
+def greet(name, age):
+    print(f'你好{name},今年{age}')
+
+greet('Anna', 23)
+# greet('Anna')  #报缺少参数错误  TypeError: greet() missing 1 required positional argument: 'age'
+# greet('anna',23,12)  #报错  参数 多余错误
+
+
+# 2.关键字参数传参
+"""
+关键字参数（Keyword Arguments）
+
+调用时用 形参名=值 的形式。
+优点：顺序无关，可读性强；可以跳过默认参数。
+"""
+def student(name,age):
+    print(f'同学名字:{name}，年龄:{age}')
+
+student(name='Anna', age=23)
+student(age=23,name='TIM') #关键子调用顺序可以随意写
+
+# 3.默认参数
+"""
+默认参数（Default Arguments）
+1.定义时指定默认值：def func(a, b=10):
+2.默认值在函数定义时计算一次（重要陷阱！）。
+3.调用时可省略，提供时覆盖默认值。
+"""
+def add(a,b=3):
+    return a+b
+
+print(add(1)) #4 (默认用 b=3)
+print(add(1,2)) #3 (覆盖默认)
+
+# 注意：经典的陷阱题：可变默认值共享
+def bad_append(item,lst=[]):
+    lst.append(item)
+    return lst
+
+print(bad_append(1)) #[1]
+print(bad_append(2)) #[1,2]。← 共享了同一个列表
+print(bad_append(3,[])) #[3] 提供了新的列表没问题
+
+# 正确的写法
+def bad_append2(item,lst=None):
+    if lst is None:
+        lst = []  #每次创建新列表
+    lst.append(item)
+    return lst
+
+print(bad_append2(1)) #[1]
+print(bad_append2(2)) #[2] ← 独立
+print(bad_append2(3,[])) #[3] ← 独立
+
+
+# 4.可变长参数
+"""
+3. 可变长参数（Variable-Length Arguments）
+(1) *args：可变位置参数
+1.收集多余的位置实参成 tuple。
+2.名字通常叫 args，但可以自定义（如 *numbers）
+"""
+def sum_all(*args):
+    print('args类型：',type(args))
+    return sum(args)
+
+print(sum_all(1,2,3)) #6
+print(sum_all(1,2,3,4,5,6)) #16
+print(sum_all()) #0(空 tuple)
+
+# 混用
+def func(a,b,*c):
+    print(a,b,c)
+
+func(1,2,3,4,5,6) #结果 1 2 (3,4,5,6)
+
+
+# 5.可变关键字参数
+"""
+**kwargs：可变关键字参数
+1.收集多余的关键字实参成 dict。
+2.名字通常叫 kwargs。
+"""
+def print_info(**kwargs):
+    print('kwargs 类型:',type(kwargs))
+    for k,v in kwargs.items():
+        print(f'{k},{v}')
+
+print_info(name='Anna', age=23,city='biejin')
+# 输出：
+# kwargs 类型: <class 'dict'> 字典
+# name,Anna
+# age,23
+# city,biejin
+
+# 混用 位置参数 ，可变长参数，可变关键词参数
+
+def func_hun(a,*b,**c):
+    print(a,b,c)
+
+func_hun(1,2,3,4,name='Anna',age=23,city='biejin') #结果：1 (2, 3, 4) {'name': 'Anna', 'age': 23, 'city': 'biejin'}
+
+
+"""
+参数定义顺序规则 （非常重要！）
+python 严格要求形参顺序：
+  def func(pos1, pos2, /, positional_or_keyword, *, keyword_only):
+      pass
+      
+      完整顺序（从左到右）：
+        1.位置或关键字参数（普通参数，可位置或关键字调用）。
+        2./ 前：仅位置参数（Positional-Only，Python 3.8+，如内置函数 pow(x, y, /)）。
+        3.默认参数（可省略）。
+        4.*args（可变位置）。
+        5.仅关键字参数（Keyword-Only：在 * 之后，无默认或有默认）。
+        6.**kwargs（可变关键字）。
+标准常见顺序：
+def func(a, b=10, *args, c, d=20, **kwargs):  # c, d 是仅关键字（在 *args 后）
+    ...
+
+判断：顺序是否对：
+func(1, 2, 3, 4, c=5, x=10)  # a=1(位置), b=2(位置), args=(3,4), c=5(必须关键字), kwargs={'x':10}
+# func(1, 2, 3, 4, 5)  # 报错：c 缺少
+"""
