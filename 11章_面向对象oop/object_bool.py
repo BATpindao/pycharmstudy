39d89ed640373b9c09466f3ae2de2cba3c7c96d7
apple <1483258276@qq.com>
2026-02-03 19:38:20 +0800

    python - 面向对象初识(oop)

---
+++ 11章_面向对象oop/object_bool.py
@@ -0,0 +1,11 @@
+"""
+python一切皆为对象，所有对象都有一个布尔值，通过内置函数bool()可以获取对象的布尔值
+"""
+print('布尔值：',bool(False))
+print('数值:',bool(0))
+print('空对象:',bool(None))
+print('空字符串：',bool(''))
+print('空列表：',bool([]))
+print('空字典：',bool({}))
+print('空元组：',bool(()))
+print('空集合：',bool(set()))
