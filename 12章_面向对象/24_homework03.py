37e07a021294b56a23be84163dea12bc3f20ef38
apple <1483258276@qq.com>
2026-03-08 01:55:56 +0800

    面向对象--练习题03

--- 12章_面向对象/24_homework03.py
+++ 12章_面向对象/24_homework03.py
@@ -3,6 +3,8 @@
 1.编写Doctor类，属性：name， age， job， gender， sal 5个参数的构造器，重写__eq__()方法，
 2.并判断测试类中创建的两个对象是否相等（相等就是判断属性是否相同）。
 """
+
+
 class Doctor:
     def __init__(self, name, age, job, gender):
         self.name = name
@@ -11,12 +13,17 @@ class Doctor:
         self.gender = gender
 
     def __eq__(self, other):
-        if self.name == other.name and self.age == other.age and self.job == other.job and self.gender == other.gender:
-            return True
-        else:
+        if not isinstance(other, Doctor):
             return False
+        return (
+                    self.name == other.name and
+                    self.age == other.age and
+                    self.job == other.job and
+                    self.gender == other.gender)
 
-d1 = Doctor('job',12,'java','男')
-d2 = Doctor('tom',14,'java','男')
 
+d1 = Doctor('job', 12, 'java', '男')
+d2 = Doctor('tom', 14, 'java', '男')
+
+print('123'.center(32, '-'))
 print(d1.__eq__(d2))
