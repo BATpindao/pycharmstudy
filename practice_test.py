489d099cf3e604f00d85e11f4170c97a58addf6e
apple <1483258276@qq.com>
2026-06-14 11:30:45 +0800

    协程 coroutine asyncio

--- practice_test.py
+++ practice_test.py
@@ -1,1 +1,14 @@
-f = open('/Users/apple/pandas_excel/log.txt','w',encoding='utf-8')
+import asyncio
+
+async def work():
+    print('work开始工作')
+    print('work执行中。。')
+    print('work结束工作')
+    return '工作结果'
+
+
+coroutine_retuen = work()
+
+retule = asyncio.run(coroutine_retuen)
+
+print(retule)
